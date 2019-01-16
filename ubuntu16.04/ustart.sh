81c658cfa6c3154b78debcbb1d94ab36cf589ba7
japson1987 <182886016@qq.com>
2019-01-16 11:34:44 +0800

    tutorial images

--- ubuntu16.04/ustart.sh
+++ ubuntu16.04/ustart.sh
@@ -4,4 +4,4 @@
 bootnode_addr=cnode://"$(grep cnode bootnode.log|tail -n 1|awk -F '://' '{print $2}')"
 ./build/bin/cypher --datadir db init genesis.json
 echo "bootnode地址：" $bootnode_addr
-./build/bin/cypher  --datadir db --networkid 123666 --onetport 7000 --port 16062 --rpcport 18002 --verbosity 4 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --bootnodes $bootnode_addr --rpcapi eth,web3,personal console
+./build/bin/cypher  --datadir db --networkid 123666 --onetport 7000 --port 16062 --rpcport 18002 --verbosity 3 --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --bootnodes $bootnode_addr --rpcapi eth,web3,personal console
