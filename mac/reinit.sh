9b101e0d540b3eff53a906323e57c0693c3ee41e
japson1987 <182886016@qq.com>
2019-01-22 20:53:54 +0800

    mac g4

--- mac/reinit.sh
+++ mac/reinit.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-killall -9 cypher
+
+git pull
+make cypher
+make bootnode
+systemctl stop cypher.service
+systemctl stop boot.service
+sleep 2
+rm -rf /root/.ethash/*
 ./run.sh cleandb 1
 ./run.sh init 1
+
+systemctl start cypher.service
+systemctl start boot.service
 sleep .5
+
+systemctl status cypher.service -l
+systemctl status boot.service -l
+sleep .5
+
 ./build/bin/cypher attach chaindb/1/cypher.ipc
