9390c2a28a9e2198e82ec657a11313ce9b94c6b0
japson1987 <182886016@qq.com>
2019-01-23 19:30:00 +0800

    urestart.sh

---
+++ ubuntu16.04/urestart.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+killall -9 cypher
+bootnode_addr=cnode://"$(grep cnode bootnode.log|tail -n 1|awk -F '://' '{print $2}')"
+sudo rm -rf db
+./build/bin/cypher --datadir db init genesis.json
+echo "bootnode地址：" $bootnode_addr
+./build/bin/cypher --verbosity 0 --datadir db --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --networkid 123666 --onetport 7000 --port 16062 --rpcport 18002  --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --bootnodes $bootnode_addr --rpcapi eth,web3,personal console
