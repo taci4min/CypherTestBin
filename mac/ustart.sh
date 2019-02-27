74558afd8eb16c6f133222e799ca4646207b089b
japson1987 <182886016@qq.com>
2019-02-27 09:56:49 +0800

    fix permission denied for linux

--- mac/ustart.sh
+++ mac/ustart.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 NODE_DIR="db"
-bootnode_addr=cnode://"$(grep cnode ../bootnode.log|tail -n 1|awk -F '://' '{print $2}')"
-./cypher --datadir "$NODE_DIR" init ../genesis.json
+BOOTDIR="../bootnode.log"
+GENESISDIR="../genesis.json"
+BINDIR="./cypher"
+bootnode_addr=cnode://"$(grep cnode $BOOTDIR|tail -n 1|awk -F '://' '{print $2}')"
+$BINDIR --datadir "$NODE_DIR" init $GENESISDIR
 echo "bootnode地址：" $bootnode_addr
-./cypher  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity 0 --datadir db --networkid 123551 --gcmode archive --bootnodes $bootnode_addr console 2>"$NODE_DIR/output.log"
+$BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity 0 --datadir db --networkid 123551 --gcmode archive --bootnodes $bootnode_addr console 2>"$NODE_DIR/output.log"
