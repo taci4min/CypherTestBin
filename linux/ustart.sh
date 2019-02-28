10468a40103ea331864c35aa2f67982549632fc5
japson1987 <182886016@qq.com>
2019-02-28 10:03:45 +0800

    fix permission denied for linux

--- linux/ustart.sh
+++ linux/ustart.sh
@@ -6,4 +6,4 @@ BINDIR="./cypher"
 bootnode_addr=cnode://"$(grep cnode $BOOTDIR|tail -n 1|awk -F '://' '{print $2}')"
 $BINDIR --datadir "$NODE_DIR" init $GENESISDIR
 echo "bootnode地址：" $bootnode_addr
-$BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity 0 --datadir db --networkid 123551 --gcmode archive --bootnodes $bootnode_addr console 2>"$NODE_DIR/output.log"
+$BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity 0 --datadir db --networkid 123550 --gcmode archive --bootnodes $bootnode_addr console 2>"$NODE_DIR/output.log"
