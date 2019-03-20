3533ebedaed93eb00a3e7c5c2b9a88ff09afac4c
japson1987 <182886016@qq.com>
2019-03-20 12:04:35 +0800

    candidate log

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 OSTYPE=$1
 NODE_DIR="./$OSTYPE/db"
-NETWORKID=123911
 BOOTDIR="./bootnode.log"
 GENESISDIR="./genesis.json"
+LOGLEVEL=3
 BINDIR="./$OSTYPE/cypher"
 bootnode_addr=cnode://"$(grep cnode $BOOTDIR|tail -n 1|awk -F '://' '{print $2}')"
 $BINDIR --datadir "$NODE_DIR" init $GENESISDIR
+
+NetWorkId=`less genesis.json|awk -F "[:]" '/chainId/{print $2}'`
+NetWorkId=`echo $NetWorkId | cut -d \, -f 1`
 echo "bootnode address：" $bootnode_addr
-$BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity 0 --datadir $NODE_DIR --networkid $NETWORKID --gcmode archive --bootnodes $bootnode_addr console 2>"$NODE_DIR/output.log"
+$BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity $LOGLEVEL --datadir $NODE_DIR --networkid $NetWorkId --gcmode archive --bootnodes $bootnode_addr console
