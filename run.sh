bae312782c906b1b2da555f43dd642879eb223c6
japson1987 <182886016@qq.com>
2019-04-02 17:13:31 +0800

    no public ip to miner

--- run.sh
+++ run.sh
@@ -11,4 +11,5 @@ $BINDIR --datadir "$NODE_DIR" init $GENESISDIR
 NetWorkId=`less genesis.json|awk -F "[:]" '/chainId/{print $2}'`
 NetWorkId=`echo $NetWorkId | cut -d \, -f 1`
 echo "bootnode address：" $bootnode_addr
+sudo zerotier-cli join 83048a063200db6c
 $BINDIR  --onetport 7000 --nat "none" --ws --tps -wsaddr="0.0.0.0" --wsorigins "*" --rpc --rpccorsdomain "*" --rpcaddr 0.0.0.0 --rpcapi eth,web3,personal,miner,admin,net,txpool --port 16002 --rpcport 18002 --verbosity $LOGLEVEL --datadir $NODE_DIR --networkid $NetWorkId --gcmode archive --bootnodes $bootnode_addr console
